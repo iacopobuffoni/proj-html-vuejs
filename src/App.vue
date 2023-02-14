7478f44b3ed8af5e7889534cf2a27b6ce09b2c3f
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-02-14 09:04:33 +0100

    Header

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+	import AppHeader from './components/Header/AppHeader.vue';
+	import AppMain from './components/Main/AppMain.vue';
+	import AppFooter from './components/Footer/AppFooter.vue';
+	export default {
+		name: 'App',
+  		components : {
+			AppHeader,
+			AppMain,
+			AppFooter
+
+  	}
+	
+}
+</script>
+
+<template>
+	
+	<AppHeader/>
+	<AppMain/>
+	<AppFooter/>
+
+</template>
+
+<style lang="scss">
+	@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
+	@import "styles/main";
+
+	body {
+		font-family: 'DM Sans', sans-serif;
+	}
+</style>
