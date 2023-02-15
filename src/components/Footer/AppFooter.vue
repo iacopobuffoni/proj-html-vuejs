ce697363933337594bd5d5b55431cf9d08c3861a
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-02-15 17:33:20 +0100

    Finito, passo alle varie migliorie

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -5,11 +5,123 @@ export default {
 </script>
 
 <template>
-    <footer>
-        FOOTER
-    </footer>
+    <main>
+        <footer>
+            <div class="footer-container">
+                <div class="left-footer-container">
+                    <h2> Grow Your Online Business Strategically, and Improve Customer retention. </h2>
+                    <font-awesome-icon icon="fa-brands fa-facebook-f" class="fs-5 me-4 mt-4" />
+                    <font-awesome-icon icon="fa-brands fa-twitter" class="fs-5 me-4 mt-4" />
+                    <font-awesome-icon icon="fa-brands fa-instagram" class="fs-5 me-4 mt-4" />
+                    <font-awesome-icon icon="fa-brands fa-youtube" class="fs-5 me-4 mt-4" />
+                    <font-awesome-icon icon="fa-brands fa-linkedin-in" class="fs-5 me-4 mt-4" />
+                    <font-awesome-icon icon="fa-brands fa-tiktok" class="fs-5 me-4 mt-4" />
+                </div>
+                <div class="right-footer-container">
+                    <div class="list fs-5">
+                        <h6 class="fs-5"> Services </h6>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    Marketing Plan
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Sales Development
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Digital Marketing
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Pricing
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Why Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Case Studies
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="list fs-5">
+                        <h6 class="fs-5"> Resources </h6>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    Learning Center
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Video Tutorials
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Customers
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Blog
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="list fs-5">
+                        <h6 class="fs-5"> Company </h6>
+                        <ul>
+                            <li>
+                                <a href="">
+                                    Who We Are
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Contact Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    Careers
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-container pt-5">
+                <div class="left-footer-container">
+                    <p class="padding">
+                        <font-awesome-icon icon="fa-regular fa-copyright" /> 2020 &#x2022; Avada Consultant &#x2022; Powered by IacopoBuffoni
+                    </p>
+                </div>
+                <div class="right-footer-container">
+                    <div class="list">
+                        <p> Call Us (555) 802-134 </p>
+                    </div>
+                    <div class="list">
+
+                    </div>
+                    <div class="list">
+                        <p class=""> info@yourcompany.com </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </main>
 </template>
 
 <style lang="scss" scoped>
-@import "../../styles/footer/appfooter.scss"; 
+@import "../../styles/footer/appfooter.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -9,9 +9,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
 import { faPhone, faAngleDown, faArrowRight, faArrowUpRightDots, faPeopleGroup, faChartPie } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faInstagram, faLinkedinIn, faTiktok, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
 
 /* add icons to the library */
-library.add(faPhone, faAngleDown, faArrowRight, faArrowUpRightDots, faPeopleGroup, faChartPie)
+library.add(faPhone, faAngleDown, faArrowRight, faArrowUpRightDots, faPeopleGroup, faChartPie, faFacebookF, faTwitter, faInstagram, faYoutube, faLinkedinIn, faTiktok, faCopyright)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
